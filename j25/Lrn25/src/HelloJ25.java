b4944361a5aa640c7d137f0918852a5af92ccc96
khatami <khatami@caspco.ir>
2025-11-04 18:00:06 +0330

    initialize

---
+++ j25/Lrn25/src/HelloJ25.java
@@ -0,0 +1,13 @@
+/**
+ * @author Hamid Khatami(khatami@caspco.ir)
+ * @version 1.0 2025.1104
+ * @since 1.0
+ *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class HelloJ25 {
+
+
+  public void main(String[] args){
+    System.out.println("Hello World");
+  }
+}
